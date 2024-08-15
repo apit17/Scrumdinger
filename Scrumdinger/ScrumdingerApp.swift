1ac5a5d83a054cafdd0e10919f8609c77a9e5eee
apit <apit.gilang@iclou>
2024-08-15 14:14:58 +0700

    init commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Apit on 15/08/24.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
